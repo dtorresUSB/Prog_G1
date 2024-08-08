4048da26d03a9591cad7e891d21b3e4290662198
dtorresUSB <dtorres@usbbog.edu.co>
2024-08-08 17:45:26 -0500

    sesion2

---
+++ README.md
@@ -0,0 +1,2 @@
+Repositorio del curso de programación
+Puede descargar el código para que pueda ejecutarlo en su propio computador.
---
+++ Sesion2/tipo.py
@@ -0,0 +1,26 @@
+#------------------Conocer el tipo de variable------------------
+x=5
+print(type(x))
+#------------------Solicitar datos a un usuario------------------
+x=int(input('Ingrese un numero: '))
+y=x+2
+print(y)
+#-------------------------Comentarios----------------------------
+#para comentar un codigo se debe utilizar el simbolo "#" al inicio
+#de la linea.
+#puede utilizar los comandos "ctrl+k" seguido de "ctrl+c" para comentar
+#puede utilizar los comandos "ctrl+k" seguido de "ctrl+u" para quitar
+#el comentario.
+#tambien puede utilizar """ al inicio y """ al final
+
+#------------Programa identificar numeros pares e impares--------
+x=int(input('Ingrese un numero: '))
+if x%2==0:
+    print('El numero es PAR')
+else:
+    print('El numero es IMPAR')
+#-------------------Programa area de un circulo--------------------
+pi=3.14
+r=float(input('Ingrese el valor del radio: '))
+A=pi*r**2
+print('El area es',A)
